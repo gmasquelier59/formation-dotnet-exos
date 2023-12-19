01b17c4937c5eac227c7c4cef8aa388677779033
gmasquelier <gmasquelier@gmail.com>
2023-12-19 16:39:33 +0100

    feat: exercice 1

--- Demo01/Program.cs
+++ Demo01/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("*** Ma liste de todo ***\nAujourd'hui je dois faire:\n\t-Apprendre le C#\n\t-Apprendre à utiliser Visual Studio\n\t-Comprendre l'affichage \"Console\"\n\t-Créer mon répertoire \"c:\\MesExercices\\\" pour les ranger\n\t-Apprécier les fonctionnalités du C#\n\n\n\n");
