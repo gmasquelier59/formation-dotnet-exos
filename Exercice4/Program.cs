a80b2f267a2058b5acba9e88c6b527a041273bb7
gmasquelier <gmasquelier@gmail.com>
2023-12-20 12:29:20 +0100

    fix: type with age = 1

--- Exercice4/Program.cs
+++ Exercice4/Program.cs
@@ -4,4 +4,4 @@ Console.Write("Veuillez saisir votre prénom : ");
 string firstName = Console.ReadLine();
 Console.Write("Veuillez saisir votre âge : ");
 int age = int.Parse(Console.ReadLine());
-Console.WriteLine("Bonjour " + firstName + " " + lastName + ", vous avez " + age + " ans");
+Console.WriteLine("Bonjour " + firstName + " " + lastName + ", vous avez " + age + " an" + (age > 1 ? 's' : '\0'));
