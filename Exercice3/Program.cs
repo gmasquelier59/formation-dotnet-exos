81483bef52c2e86ebab08a63325e1192f3651285
gmasquelier <gmasquelier@gmail.com>
2023-12-20 10:20:22 +0100

    feat: Exercice 3

---
+++ Exercice3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Veuillez saisir votre nom : ");
+string lastName = Console.ReadLine();
+Console.Write("Veuillez saisir votre prénom : ");
+string firstName = Console.ReadLine();
+Console.WriteLine("Bonjour " + lastName + " " + firstName);
