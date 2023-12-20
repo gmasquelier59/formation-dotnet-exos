5d24f65aa5dcbac7de8e4c275accfefb204bad77
gmasquelier <gmasquelier@gmail.com>
2023-12-20 12:29:54 +0100

    feat: exercice 8

---
+++ Exercice8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Entrez le prix HT de l'objet (en Euros) : ");
+double price = double.Parse(Console.ReadLine());
+
+Console.Write("Entrez le taux de TVA (en %) : ");
+double rate = double.Parse(Console.ReadLine());
+
+double tva = Math.Round(price * (rate / 100), 2);
+Console.WriteLine($"Le montant de la TVA est de {tva} Euros");
+Console.WriteLine($"Le montant de la TVA est de {price + tva} Euros");
