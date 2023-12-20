a5b00c967e15dff385a5d7db334e068270731532
gmasquelier <gmasquelier@gmail.com>
2023-12-20 16:47:28 +0100

    feat: exercice 10

---
+++ Exercice10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("--- La lettre est-elle une voyelle ? ---");
+
+Console.Write("Entrez une lettre : ");
+string letter = Console.ReadLine().ToUpper();
+if (letter.Length != 1)
+{
+    Console.WriteLine("Vous avez saisi trop de lettres !");
+}
+else
+{
+    Console.WriteLine("AEIOUY".Contains(letter) ? "Cette lettre est une voyelle !" : "Cette lettre n'est pas une voyelle !");
+}
