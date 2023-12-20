7553ef9a174da753986d47645572095d53b36f91
gmasquelier <gmasquelier@gmail.com>
2023-12-20 10:20:40 +0100

    feat: exercice 5

---
+++ Exercice5/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Veuillez saisir un nombre : ");
+double number1 = double.Parse(Console.ReadLine());
+Console.Write("Veuillez saisisr un nombre : ");
+double number2 = double.Parse(Console.ReadLine());
+double result = number1 + number2;
+Console.WriteLine($"La somme de ces deux nombres est : {result}");
