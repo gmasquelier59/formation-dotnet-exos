8769c3c906814158d31af8d4e61e213cafdecf79
gmasquelier <gmasquelier@gmail.com>
2023-12-20 12:29:38 +0100

    feat:: exercice 7

---
+++ Exercice7/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("--- Calcul de la longueur de l'hypothénuse ---");
+
+Console.Write("Entrez la longueur du premier côté (en cm) : ");
+double length1 = double.Parse(Console.ReadLine());
+
+Console.Write("Entrez la longueur du second côté (en cm) : ");
+double length2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"La longueur de l'hypothénuse est : { Math.Round( Math.Sqrt(Math.Pow(length1, 2) + Math.Pow(length2, 2)), 2) } cm");
