02d204d4f13a271ad173ddf1e148221003ba97c7
gmasquelier <gmasquelier@gmail.com>
2023-12-20 16:47:39 +0100

    feat: exercice 11

---
+++ Exercice11/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("--- Le nombre est-il divisible par... ? ---");
+
+Console.Write("Entrez un chiffre/nombre entier : ");
+if (!int.TryParse(Console.ReadLine(), out int numerateur))
+{
+    Console.WriteLine("Ce n'est pas un chiffre, ou ce n'est pas un chiffre entier !");
+    return;
+}
+
+Console.Write("Entrez un chiffre/nombre diviseur : ");
+if (!int.TryParse(Console.ReadLine(), out int denominateur))
+{
+    Console.WriteLine("Ce n'est pas un chiffre, ou ce n'est pas un chiffre entier !");
+    return;
+}
+
+Console.WriteLine((numerateur % denominateur == 0) ? $"Le chiffre/nombre est divisible par {denominateur}" : $"Le chiffre/nombre n'est pas divisible par {denominateur}");
