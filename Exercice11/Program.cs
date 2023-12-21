355cbb12124c98891e3dad35cac508a01a39a4f7
gmasquelier <gmasquelier@gmail.com>
2023-12-21 10:48:15 +0100

    fix: error output

--- Exercice11/Program.cs
+++ Exercice11/Program.cs
@@ -3,15 +3,21 @@
 Console.Write("Entrez un chiffre/nombre entier : ");
 if (!int.TryParse(Console.ReadLine(), out int numerateur))
 {
-    Console.WriteLine("Ce n'est pas un chiffre, ou ce n'est pas un chiffre entier !");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Error.WriteLine("Ce n'est pas un nombre entier !");
+    Console.ResetColor();
     return;
 }
 
 Console.Write("Entrez un chiffre/nombre diviseur : ");
 if (!int.TryParse(Console.ReadLine(), out int denominateur))
 {
-    Console.WriteLine("Ce n'est pas un chiffre, ou ce n'est pas un chiffre entier !");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Error.WriteLine("Ce n'est pas un nombre entier !");
+    Console.ResetColor();
     return;
 }
 
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine((numerateur % denominateur == 0) ? $"Le chiffre/nombre est divisible par {denominateur}" : $"Le chiffre/nombre n'est pas divisible par {denominateur}");
+Console.ResetColor();
