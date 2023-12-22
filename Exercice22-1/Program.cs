0a351b9dac222fe9174f4576f305c42cc740d6ff
gmasquelier <gmasquelier@gmail.com>
2023-12-22 14:02:05 +0100

    feat: optimize table display

--- Exercice22-1/Program.cs
+++ Exercice22-1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿//  Liste des couleurs d'affichage
+ConsoleColor[] colors = new ConsoleColor[] { ConsoleColor.DarkBlue, ConsoleColor.Cyan, ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Red, ConsoleColor.Magenta };
 
 Console.Write("Merci de saisir le nombre de tables : ");
 int nbTables;
@@ -22,25 +23,28 @@ Console.WriteLine();
 
 int largeurColonne = (nbTables * nbColonnes).ToString().Length;
 
-string sousBarre = string.Concat(Enumerable.Repeat("-", largeurColonne));
-string barre = "+" + string.Concat(Enumerable.Repeat(sousBarre + "+", nbColonnes));
+string sousBarre = string.Concat(Enumerable.Repeat("═", largeurColonne));
+string barreHaute = "╔" + string.Concat(Enumerable.Repeat(sousBarre + "╦", nbColonnes)).TrimEnd('╦') + "╗";
+string barreIntermediaire = "╠" + string.Concat(Enumerable.Repeat(sousBarre + "╬", nbColonnes)).TrimEnd('╬') + "╣";
+string barreBasse = "╚" + string.Concat(Enumerable.Repeat(sousBarre + "╩", nbColonnes)).TrimEnd('╩') + "╝";
 
-ConsoleColor[] colors = new ConsoleColor[] { ConsoleColor.DarkBlue, ConsoleColor.Cyan, ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Red, ConsoleColor.Magenta };
-
+Console.WriteLine(barreHaute);
 for (int ligne = 1; ligne <= nbTables; ligne++)
 {
-    Console.WriteLine(barre);
+    if (ligne > 1)
+        Console.WriteLine(barreIntermediaire);
 
     int colorIndex = ligne - 1;
     for (int colonne = 1; colonne <= nbColonnes; colonne++)
     {
-        Console.ForegroundColor = colors[colorIndex % (colors.Length)];
-        Console.Write("|" + (ligne * colonne).ToString().PadLeft(largeurColonne));
+        Console.Write('║');
+        Console.ForegroundColor = colors[colorIndex % colors.Length];
+        Console.Write((ligne * colonne).ToString().PadLeft(largeurColonne));
+        Console.ResetColor();
         colorIndex++;
     }
-    Console.WriteLine("|");
+    Console.WriteLine("║");
 
     Console.ResetColor();
 }
-
-Console.WriteLine(barre);
+Console.WriteLine(barreBasse);
