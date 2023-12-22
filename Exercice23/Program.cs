b57d294cbe5bca8e685c588ad739eae2473c8aa9
gmasquelier <gmasquelier@gmail.com>
2023-12-22 14:02:27 +0100

    refactor

--- Exercice23/Program.cs
+++ Exercice23/Program.cs
@@ -1,10 +1,16 @@
 ﻿Console.WriteLine("--- Acroissement de population ---\n");
 
-int annee = 2015;
+const double populationInitale = 96809;
+const double populationAAtteindre = 120000;
+const int anneeInitiale = 2015;
+const double tauxAcroissement = 0.89;
+
+int annee = anneeInitiale;
 double population;
-for(population = 96809; population <= 120000; population += (population * 0.89) / 100)
+for(population = populationInitale; population <= populationAAtteindre; population += (population * tauxAcroissement) / 100)
 {
     annee++;
 }
-Console.WriteLine($"Il faudra {annee - 2015} ans, nous serons en {annee}");
-Console.WriteLine($"Il y aura {Math.Ceiling(population)} habitants en {annee}");
+Console.WriteLine($"A Tourcoing, en {anneeInitiale}, avec une population de départ de {populationInitale:N0} habitants et un taux d'acroissement annuel de {tauxAcroissement:N}%, pour atteindre {populationAAtteindre:N0} habitants :\n");
+Console.WriteLine($"\t- il faudra {annee - anneeInitiale} ans, nous serons en {annee}");
+Console.WriteLine($"\t- il y aura {Math.Ceiling(population):N0} habitants en {annee}");
