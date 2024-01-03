f588f76068cfbe90b33a6a13673f6fcc784fa05a
gmasquelier <gmasquelier@gmail.com>
2024-01-03 14:31:56 +0100

    feat: exercice43

---
+++ Exercice43/Program.cs
@@ -0,0 +1,23 @@
+﻿int compter_lettre_a(string chaine)
+{
+    int count = 0;
+
+    foreach(char lettre in chaine)
+    {
+        if (lettre == 'a')
+            count++;
+    }
+
+    return count;
+}
+
+int compter_lettre_a_bis(string chaine)
+{
+    return chaine.Count(x => x == 'a');
+}
+
+Console.WriteLine(compter_lettre_a("C'est le b-a ba"));
+Console.WriteLine(compter_lettre_a("mixer"));
+
+Console.WriteLine(compter_lettre_a_bis("C'est le b-a ba"));
+Console.WriteLine(compter_lettre_a_bis("mixer"));
