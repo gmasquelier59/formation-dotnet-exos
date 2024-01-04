0bbdf56f7064a8e00de69465cd7b58c127762662
gmasquelier <gmasquelier@gmail.com>
2024-01-04 13:41:22 +0100

    build: réorganisation solutions

--- Fundamentals/Exercice45/Program.cs
+++ Fundamentals/Exercice45/Program.cs
@@ -1,4 +1,4 @@
-﻿(double, double, double, double) operation(double a, double b)
+﻿(double somme, double difference, double quotien, double produit) Operation(double a, double b)
 {
     return (a + b, a - b, a / b, a * b);
 }
@@ -8,7 +8,7 @@ int.TryParse(Console.ReadLine(), out int nombreA);
 Console.Write("Nombre B : ");
 int.TryParse(Console.ReadLine(), out int nombreB);
 
-(double somme, double difference, double quotient, double produit) = operation(nombreA, nombreB);
+(double somme, double difference, double quotient, double produit) = Operation(nombreA, nombreB);
 
 Console.WriteLine($"Somme : {somme}");
 Console.WriteLine($"Différence : {difference}");
