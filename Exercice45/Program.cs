e564826f8e7d6f5d0ef2d9183ffbddc36ac12c21
gmasquelier <gmasquelier@gmail.com>
2024-01-04 09:14:08 +0100

    feat: Exercice45

---
+++ Exercice45/Program.cs
@@ -0,0 +1,16 @@
+﻿(double, double, double, double) operation(double a, double b)
+{
+    return (a + b, a - b, a / b, a * b);
+}
+
+Console.Write("Nombre A : ");
+int.TryParse(Console.ReadLine(), out int nombreA);
+Console.Write("Nombre B : ");
+int.TryParse(Console.ReadLine(), out int nombreB);
+
+(double somme, double difference, double quotient, double produit) = operation(nombreA, nombreB);
+
+Console.WriteLine($"Somme : {somme}");
+Console.WriteLine($"Différence : {difference}");
+Console.WriteLine($"Quotient : {quotient}");
+Console.WriteLine($"Produit : {produit}");
