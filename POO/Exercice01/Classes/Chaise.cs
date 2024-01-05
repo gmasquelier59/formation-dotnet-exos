fabb815505a09bd3fcf19a8cc22dd837771a4974
gmasquelier <gmasquelier@gmail.com>
2024-01-05 08:57:24 +0100

    feat: Exercice01

---
+++ POO/Exercice01/Classes/Chaise.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice01.Classes
+{
+    internal class Chaise
+    {
+        private int _nbPieds;
+
+        public int NbPieds
+        {
+            get => _nbPieds;
+            set
+            {
+                if (value < 1 || value > 10)
+                    throw new ArgumentOutOfRangeException(nameof(value), "Le nombre de pieds doit être compris entre 1 et 10");
+                _nbPieds = value;
+            }
+        }
+        public string Materiau { get; set; }
+        public string Couleur { get; set; }
+
+        /// <summary>
+        /// Instancie une chaise avec 4 pieds en Bois de couleur Blanche
+        /// </summary>
+        public Chaise() {
+            NbPieds = 4;
+            Materiau = "Bois";
+            Couleur = "Blanche";
+        }
+
+        public Chaise(int nbPieds, string materiau, string couleur)
+        {
+            NbPieds = nbPieds;
+            Materiau = materiau;
+            Couleur = couleur;
+        }
+
+        public override string ToString()
+        {
+            return $"Je suis une {this.GetType().Name}, avec {NbPieds} pied(s) en {Materiau} et de couleur {Couleur}";
+        }
+    }
+}
--- POO/Exercice01/Program.cs
+++ POO/Exercice01/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿using Exercice01.Classes;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Chaise> chaises = new List<Chaise>();
+
+        chaises.Add(new Chaise());
+        chaises.Add(new Chaise(2, "Métal", "Noire"));
+        chaises.Add(new Chaise(6, "Plastique", "Bleues"));
+
+        foreach(Chaise chaise in chaises)
+        {
+            Console.WriteLine(chaise);
+        }
     }
 }
