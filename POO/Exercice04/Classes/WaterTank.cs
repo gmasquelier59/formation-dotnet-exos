b6b8a4b80b4832a15ae2d196e030c2740df09f34
gmasquelier <gmasquelier@gmail.com>
2024-01-05 16:30:30 +0100

    feat: Exercice04

---
+++ POO/Exercice04/Classes/WaterTank.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice04.Classes
+{
+    internal class WaterTank
+    {
+        private int _fillLevel = 0;
+
+        public int Number { get; private set; }
+
+        public int WeightEmpty { get; private set; }
+
+        public int MaxCapacity { get; private set; }
+
+        public int FillLevel
+        {
+            get => _fillLevel;
+            private set
+            {
+                if (value > MaxCapacity)
+                    value = MaxCapacity;
+
+                if (value < 0)
+                    value = 0;
+
+                TotalFillLevels = TotalFillLevels - FillLevel + value;
+                _fillLevel = value;
+            }
+        }
+
+        public int TotalWeight => WeightEmpty + FillLevel;
+
+        public static int TotalFillLevels { get; private set; } = 0;
+
+        public static int TotalNumberOfTanks { get; private set; } = 0;
+
+        public WaterTank(int weightEmpty, int maxCapacity, int fillLevel)
+        {
+            TotalNumberOfTanks++;
+            Number = TotalNumberOfTanks;
+            WeightEmpty = weightEmpty;
+            MaxCapacity = maxCapacity;
+            FillLevel = fillLevel;
+        }
+
+        public int Fill(int waterQuantity)
+        {
+            if (waterQuantity < 0)
+                return 0;
+
+            if (FillLevel + waterQuantity <= MaxCapacity)
+            {
+                FillLevel += waterQuantity;
+                return 0;
+            }
+
+            int excessAmount = (FillLevel + waterQuantity) - MaxCapacity;
+            FillLevel = MaxCapacity;
+
+            return excessAmount;
+        }
+
+        public int Empty(int waterQuantity)
+        {
+            if (waterQuantity <= 0)
+                return 0;
+
+            if (waterQuantity <= FillLevel)
+            {
+                FillLevel -= waterQuantity;
+                return waterQuantity;
+            }
+
+            int waterTaken = FillLevel;
+            FillLevel = 0;
+
+            return waterTaken;
+        }
+
+        public void Show()
+        {
+            int tankHeight = MaxCapacity / 10;
+
+            Random rand = new Random();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"    Citerne n°{Number}");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("               /============================");
+            Console.WriteLine("              //");
+            Console.WriteLine("             //");
+            Console.WriteLine("╔═══════════\\ /═══════════╗");
+
+            for (int i = 0; i < (tankHeight - Math.Ceiling((double)((FillLevel * tankHeight) / (double)MaxCapacity))); i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write("║");
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write("█████████████████████████");
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                Console.WriteLine("║");
+            }
+
+            for (int i = 0; i < (FillLevel * tankHeight) / MaxCapacity; i++)
+            {
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write("║");
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                for (int j = 0; j < 25; j++)
+                    if (rand.Next(1, 8) == 1)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.Write("º");
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkCyan;
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("█");
+                    }
+
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("║");
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("╚═════════════════════════╝");
+            Console.WriteLine($"{FillLevel + " litre(s) sur " + MaxCapacity +  " - " + FillLevel * 100 / MaxCapacity + "%",19}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ POO/Exercice04/Program.cs
@@ -0,0 +1,56 @@
+﻿using Exercice04.Classes;
+
+WaterTank tank1 = new WaterTank(2, 150, 10);
+WaterTank tank2 = new WaterTank(5, 200, 89);
+
+List<WaterTank> waterTanks = new List<WaterTank>
+{
+    tank1, tank2
+};
+
+foreach (WaterTank waterTank in waterTanks)
+    Console.WriteLine($"Poids total de la citerne {waterTank.Number} : {waterTank.TotalWeight} kg (poids à vide : {waterTank.WeightEmpty} kg)");
+
+AfficherSeparateur();
+
+AfficherNiveauCiternes();
+
+AfficherSeparateur();
+
+int excessWater = tank1.Fill(60);
+Console.WriteLine($"Excès d'eau récupéré après avoir ajouté 60 litres à la citerne 1 : {excessWater} litre(s)");
+
+AfficherSeparateur();
+
+int waterTaken = tank1.Empty(10);
+Console.WriteLine($"Eau récupérée après une tentative de retrait de 10 litres sur la citerne 1 : {waterTaken} litre(s)");
+AfficherSeparateur();
+
+AfficherNiveauCiternes();
+
+waterTaken = tank1.Empty(8);
+Console.WriteLine($"Eau récupérée après une tentative de retrait de 60 litres sur la citerne 1 : {waterTaken} litre(s)");
+AfficherSeparateur();
+
+AfficherNiveauCiternes();
+AfficherSeparateur();
+Console.WriteLine();
+
+tank2.Fill(32);
+
+foreach (WaterTank waterTank in waterTanks)
+{
+    waterTank.Show();
+}
+
+void AfficherSeparateur()
+{
+    Console.WriteLine(string.Concat(Enumerable.Repeat("»", 100)));
+}
+
+void AfficherNiveauCiternes()
+{
+    foreach (WaterTank waterTank in waterTanks)
+        Console.WriteLine($"Quantité d'eau dans la citerne {waterTank.Number} : {waterTank.FillLevel} litre(s) sur {waterTank.MaxCapacity}");
+    Console.WriteLine($"Quantité d'eau dans toutes les {WaterTank.TotalNumberOfTanks} citernes : {WaterTank.TotalFillLevels} litre(s)");
+}
