7ed0be057e01884716f29328b17630c3175162c9
gmasquelier <gmasquelier@gmail.com>
2024-01-05 13:51:12 +0100

    feat: Exercice02

---
+++ POO/Exercice02/Classes/Salarie.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice02.Classes
+{
+    internal class Salarie
+    {
+        private const decimal salaireParDefaut = 16236;
+        private const decimal salaireMinimum = 1;
+        private const decimal salaireMaximum = 1000000;
+
+        private decimal _salaire;
+
+        public string Matricule { get; set; } = "";
+        public string Service { get; set; } = "";
+        public string Categorie { get; set; } = "";
+        public string Nom { get; set; }
+        public decimal Salaire
+        {
+            get => _salaire;
+            set
+            {
+                if (value < salaireMinimum || value > salaireMaximum)
+                    throw new ArgumentOutOfRangeException(nameof(value), "Le salaire doit être compris entre {salaireMinimum} et {salaireMaximum} euros");
+                SalaireTotal = SalaireTotal - Salaire + value;
+                _salaire = value;
+            }
+        }
+        public static decimal SalaireTotal { get; private set; } = 0;
+        public static int NombreSalaries { get; private set; } = 0;
+
+        public static decimal SalaireMoyen
+        {
+            get
+            {
+                if (NombreSalaries == 0)
+                    return 0;
+
+                return SalaireTotal / NombreSalaries;
+            }
+        }
+
+        public Salarie()
+        {
+            Nom = "Salarié par défaut";
+            Salaire = salaireParDefaut;
+
+            NombreSalaries++;
+        }
+
+        public Salarie(string nom, int salaire)
+        {
+            Nom = nom;
+            Salaire = salaire;
+
+            NombreSalaries++;
+        }
+
+        public static void AfficherEntreprise()
+        {
+            Console.WriteLine($"Nombre de salariés : {NombreSalaries}");
+            Console.WriteLine($"Salaire total : {SalaireTotal} euros");
+            Console.WriteLine($"Salaire moyen : {SalaireMoyen:F2} euros");
+        }
+
+        public void AfficherSalaire()
+        {
+            Console.WriteLine($"Le salaire de {Nom} est de {Salaire} euros");
+            return;
+        }
+
+        public static void RemiseAZero()
+        {
+            SalaireTotal = 0;
+            NombreSalaries = 0;
+        }
+    }
+}
---
+++ POO/Exercice02/Program.cs
@@ -0,0 +1,26 @@
+﻿using Exercice02.Classes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Salarie chloe = new Salarie("Chloé", 24000);
+        chloe.AfficherSalaire();
+        new Salarie("Emma", 30000).AfficherSalaire();
+        new Salarie("Georges", 26000).AfficherSalaire();
+        new Salarie().AfficherSalaire();
+
+        Console.WriteLine();
+        Salarie.AfficherEntreprise();
+
+        chloe.Salaire = 500000;
+
+        Console.WriteLine();
+        Salarie.AfficherEntreprise();
+
+        Salarie.RemiseAZero();
+
+        Console.WriteLine();
+        Salarie.AfficherEntreprise();
+    }
+}
