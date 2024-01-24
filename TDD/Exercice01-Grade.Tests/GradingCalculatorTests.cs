b4433eef5c3698c7f84cb0b3a1393b50296f29d8
gmasquelier <gmasquelier@gmail.com>
2024-01-24 15:24:28 +0100

    feat(tdd) : TestCaseSource

--- TDD/Exercice01-Grade.Tests/GradingCalculatorTests.cs
+++ TDD/Exercice01-Grade.Tests/GradingCalculatorTests.cs
@@ -10,6 +10,8 @@ namespace Exercice01_Grade.Tests
             _gradingCalculator = new GradingCalculator();
         }
 
+        //  Première manière de faire le test avec un TestCase
+
         [Test]
         [TestCase(95, 90, ExpectedResult = 'A')]
         [TestCase(85, 90, ExpectedResult = 'B')]
@@ -24,6 +26,28 @@ namespace Exercice01_Grade.Tests
             _gradingCalculator.AttendancePercentage = attendancePercentage;
 
             return _gradingCalculator.GetGrade();
+        }
+
+        //  Seconde manière de faire le test avec un TestCaseSource
+
+        public static object[] GradeCases =
+        {
+            new object[] { 95, 90, 'A' },
+            new object[] { 85, 90, 'B' },
+            new object[] { 65, 90, 'C' },
+            new object[] { 95, 65, 'B' },
+            new object[] { 95, 55, 'F' },
+            new object[] { 65, 55, 'F' },
+            new object[] { 50, 90, 'F' }
+        };
+
+        [TestCaseSource(nameof(GradeCases))]
+        public void Should_GetGrade_InputScoreAndPercentage_GetRightGrade_Source(int score, int attendancePercentage, char grade)
+        {
+            _gradingCalculator.Score = score;
+            _gradingCalculator.AttendancePercentage = attendancePercentage;
+
+            Assert.That(_gradingCalculator.GetGrade(), Is.EqualTo(grade));
         }
     }
 }
