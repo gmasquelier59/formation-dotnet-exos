5cf98108979b4829fd3cc65f34cfe40c6447ea9e
gmasquelier <gmasquelier@gmail.com>
2024-01-30 15:18:17 +0100

    AspNetMVC : views & controllers

---
+++ ASPDotnetCoreMVC/ASPDotnetCoreMVC-Exercice01/Models/ContactModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPDotnetCoreMVC_Exercice01.Models
+{
+    public class ContactModel
+    {
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        [Display(Name = "Résumé")]
+        public string Resume { get; set; }
+        public string Photo { get; set; }
+    }
+}
