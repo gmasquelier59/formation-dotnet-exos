a162d4507bfbb1cc0cd4263aeec8e0198b22a327
gmasquelier <gmasquelier@gmail.com>
2024-02-14 09:24:10 +0100

    Blazor : Exo2 - FormulairePersonne

---
+++ BlazorWebAssembly/Exercice02-FormulairePersonne/Pages/Fiche.razor.cs
@@ -0,0 +1,18 @@
+﻿using Exercice02_FormulairePersonne.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Exercice02_FormulairePersonne.Pages
+{
+    public class FicheBase : ComponentBase
+    {
+        public Person Person {  get; set; }
+        public bool ShowCard {  get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Person = new Person();
+            Person.LastName = "MASQUELIER";
+            Person.Birthday = new DateTime(1981, 1, 23);
+        }
+    }
+}
